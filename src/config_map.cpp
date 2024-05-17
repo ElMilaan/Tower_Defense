3dc115f6c08e5112c9163d4d389685c9aaa28d88
Nolan <bourrel.nolan@gmail.com>
2024-05-17 11:43:30 +0200

    config

--- src/config_map.cpp
+++ src/config_map.cpp
@@ -3,3 +3,18 @@
 #include <string>
 #include "config_map.hpp"
 
+void config_map (){
+std::ifstream myfile;
+myfile.open("shopping_list.txt");
+std::string myline;
+if ( myfile.is_open() ) {
+while ( myfile ) { // equivalent to myfile.good()
+std::getline (myfile, myline);
+std::cout << myline << '\n';
+
+}
+}
+else {
+std::cout << "Couldn't open file\n";
+}
+}
--- src/config_map.hpp
+++ src/config_map.hpp
@@ -2,14 +2,4 @@
 #include <fstream>
 #include <string>
 
-int main () {
-std::ifstream myfile; myfile.open("config_map.itd");
-std::string mystring;
-if ( myfile.is_open() ) {
-char mychar;
-while ( myfile ) {
-mychar = myfile.get();
-std::cout << mychar;
-}
-}
-}
+void configmap (){};
